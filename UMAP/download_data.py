cc5a3f4319877475eb51cac3584862ca6a31b14e
michele <michelecocca.mc@gmail.com>
2020-04-20 18:36:02 +0200

    new dates

--- UMAP/download_data.py
+++ UMAP/download_data.py
@@ -10,11 +10,13 @@ city = sys.argv[1]
 for i in range(1, len(sys.argv)):
 
     city = sys.argv[i]
+    # init_date = datetime.datetime(2017, 11, 1, 23, 59, 59)
+    # final_date = datetime.datetime(2017, 11, 2, 23, 59, 59)
+    init_date = None
+    final_date = None
     print('Query on %s' % city)
-    init_date = datetime.datetime(2017, 11, 1, 23, 59, 59)
-    final_date = datetime.datetime(2017, 11, 2, 23, 59, 59)
-    # init_date = None
-    # final_date = None
+    print('Init date', init_date)
+    print('final date', final_date)
     data_downloader = DataDownloader(init_date, final_date)
     data_downloader.download_data(city)
     data_downloader.dump_df('../Data/', 'csv')
